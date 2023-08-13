fac609f0751047330664a8b805da72b47ab2b3aa
Md Iqbal Hossan <engiqbal1999@gmail.com>
2023-08-13 19:35:56 +0600

    database connected

--- src/route.ts
+++ src/route.ts
@@ -1,24 +1,22 @@
-// const express = require('express')
-// const app = express()
-// const port = 3000
 
-// app.get('/', (req, res) => {
-//     res.send('Hello World!')
-// })
+import mongoose from "mongoose";
+import app from "./app";
 
-// app.listen(port, () => {
-//     console.log(`Example app listening on port ${port}`)
-// })
+const port: number = 5000;
+
+main().catch(err => console.log(err))
+// database connect
+async function main() {
+    try {
+        await mongoose.connect('mongodb://127.0.0.1.27017/test')
+        console.log("Database conneciotn successful")
+        app.listen(port, () => {
+            console.log(`Example app listener on port ${port}`);
+        })
+    } catch (err) {
+        console.log(err)
+    }
+}
+main()
 
 
-const express = require('express');
-const app = express();
-const port = 3000;
-
-app.get('/', (req, res) => {
-    res.send("Hello Iqbal Hossan");
-})
-
-app.listen(port, () => {
-    console.log(`Example app listener on port ${port}`);
-})
